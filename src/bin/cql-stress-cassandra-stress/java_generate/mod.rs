98ddca9fbb3e94797e927d131ac0e6d68b54c814
muzarski <mikolaj.uzarski@scylladb.com>
2023-08-10 15:55:55 +0200

    c-s java_generate: values module
    Introduces `java_generate::values` module.
    
    This module defines the traits implemented by value generators.
    It is fully based on c-s `Generator` abstract class.
    See: https://github.com/scylladb/scylla-tools-java/blob/master/tools/stress/src/org/apache/cassandra/stress/generate/values/Generator.java.
    And one of the implementations of this class:
    https://github.com/scylladb/scylla-tools-java/blob/master/tools/stress/src/org/apache/cassandra/stress/generate/values/Bytes.java.

--- src/bin/cql-stress-cassandra-stress/java_generate/mod.rs
+++ src/bin/cql-stress-cassandra-stress/java_generate/mod.rs
@@ -1,2 +1,3 @@
 pub mod distribution;
 pub mod faster_random;
+pub mod values;
---
+++ src/bin/cql-stress-cassandra-stress/java_generate/values/mod.rs
@@ -0,0 +1,90 @@
+use super::distribution::{uniform::UniformDistribution, Distribution};
+use scylla::transport::partitioner::{Murmur3Partitioner, Partitioner};
+
+/// Generic generator of random values.
+/// Holds the distributions that the seeds and sizes are sampled from.
+/// Wraps the actual generator which makes use of the distributions.
+pub struct Generator<T: ValueGenerator> {
+    salt: i64,
+    identity_distribution: Box<dyn Distribution>,
+    size_distribution: Box<dyn Distribution>,
+    gen: T,
+}
+
+impl<T: ValueGenerator> Generator<T> {
+    pub fn new(gen: T, config: GeneratorConfig) -> Self {
+        let salt = config.salt;
+        let identity_distribution = match config.identity_distribution {
+            Some(dist) => dist,
+            None => Self::default_identity_distribution(),
+        };
+        let size_distribution = match config.size_distribution {
+            Some(dist) => dist,
+            None => Self::default_size_distribution(),
+        };
+
+        Self {
+            salt,
+            identity_distribution,
+            size_distribution,
+            gen,
+        }
+    }
+
+    pub fn set_seed(&mut self, seed: i64) {
+        self.identity_distribution.set_seed(seed ^ self.salt);
+    }
+
+    pub fn generate(&mut self) -> T::Value {
+        self.gen.generate(
+            self.identity_distribution.as_mut(),
+            self.size_distribution.as_mut(),
+        )
+    }
+
+    /// See https://github.com/scylladb/scylla-tools-java/blob/master/tools/stress/src/org/apache/cassandra/stress/generate/values/Generator.java#L59
+    fn default_identity_distribution() -> Box<dyn Distribution> {
+        Box::new(UniformDistribution::new(1.0, 100_000_000_000.0).unwrap())
+    }
+
+    /// See https://github.com/scylladb/scylla-tools-java/blob/master/tools/stress/src/org/apache/cassandra/stress/generate/values/Generator.java#L64
+    fn default_size_distribution() -> Box<dyn Distribution> {
+        Box::new(UniformDistribution::new(4.0, 8.0).unwrap())
+    }
+}
+
+/// Generator config - used to construct new Generator instance.
+pub struct GeneratorConfig {
+    salt: i64,
+    identity_distribution: Option<Box<dyn Distribution>>,
+    size_distribution: Option<Box<dyn Distribution>>,
+}
+
+impl GeneratorConfig {
+    /// As in c-s, receive some seed string which is hashed to retrieve the
+    /// salt value. See https://github.com/scylladb/scylla-tools-java/blob/master/tools/stress/src/org/apache/cassandra/stress/generate/values/GeneratorConfig.java#L39.
+    pub fn new(
+        seed_str: &str,
+        identity_distribution: Option<Box<dyn Distribution>>,
+        size_distribution: Option<Box<dyn Distribution>>,
+    ) -> Self {
+        let bytes = seed_str.as_bytes();
+        let salt = Murmur3Partitioner::hash(bytes);
+        Self {
+            salt: salt.value,
+            identity_distribution,
+            size_distribution,
+        }
+    }
+}
+
+/// The actual value Generator trait.
+pub trait ValueGenerator {
+    type Value;
+
+    fn generate(
+        &mut self,
+        identity_distribution: &mut dyn Distribution,
+        size_distribution: &mut dyn Distribution,
+    ) -> Self::Value;
+}
