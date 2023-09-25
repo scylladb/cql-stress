15b1ca8b7e80a790d61246e778d60493bf0b99d9
muzarski <mikolaj.uzarski@scylladb.com>
2023-09-25 15:51:25 +0200

    tools: replace docker with docker-compose
    `docker compose up --wait` allows us to wait until docker container is
    healthy. This is why we replace the code responsible for polling docker container
    with `docker compose` functionality in the `tools/test_with_scylla.py` script.

--- tools/test_with_scylla.py
+++ tools/test_with_scylla.py
@@ -5,44 +5,21 @@ from collections import namedtuple
 from contextlib import contextmanager
 import os
 import subprocess
-import time
 
 
 ScyllaDockerNode = namedtuple("ScyllaDockerNode", ["ip", "port"])
 
 
-def wait_until_scylla_responds(container_id, max_attempts):
-    for i in range(max_attempts):
-        try:
-            print(
-                f"Waiting until Scylla responds to queries (attempt #{i}/{max_attempts})")
-            subprocess.run(["docker", "exec", container_id,
-                            "cqlsh", "-e", "select * from system.local"],
-                           check=True, stdout=subprocess.DEVNULL)
-            # If we are here, this means success
-            return
-        except KeyboardInterrupt:
-            # Propagate it further
-            raise
-        except:
-            time.sleep(5)
-
-    raise RuntimeError(
-        "Scylla didn't start responding within the configured amount of retries")
-
-
 @contextmanager
-def scylla_docker(teardown_behavior, max_healthcheck_attempts):
+def scylla_docker(teardown_behavior, compose_yaml="docker/scylla_test/compose.yml"):
     print("Setting up Scylla")
-    cp = subprocess.run(args=["docker", "run", "-d", "-p", "9042:9042", "scylladb/scylla"],
-                        capture_output=True, check=True)
-    container_id = cp.stdout.decode('utf-8').strip()
-    print(f"Container ID: {container_id}")
+    print("Starting the container and waiting for it to be healthy")
+
+    subprocess.run(args=["docker", "compose", "-f",
+                   compose_yaml, "up", "-d", "--wait"], check=True)
 
     success = False
     try:
-        # Wait until Scylla starts responding to queries
-        wait_until_scylla_responds(container_id, max_healthcheck_attempts)
 
         yield ScyllaDockerNode(ip="127.0.0.1", port="9042")
         success = True
@@ -56,8 +33,10 @@ def scylla_docker(teardown_behavior, max_healthcheck_attempts):
 
         if do_teardown:
             print("Tearing down")
-            subprocess.run(args=["docker", "stop", container_id], check=True)
-            subprocess.run(args=["docker", "rm", container_id], check=True)
+            subprocess.run(args=["docker", "compose", "-f",
+                           compose_yaml, "stop"], check=True)
+            subprocess.run(args=["docker", "compose", "-f",
+                           compose_yaml, "down"], check=True)
             print("Teardown successful")
 
 
@@ -69,11 +48,6 @@ def main():
                         choices=["always", "on-success", "never"],
                         default="always",
                         help="configure teardown behavior")
-    parser.add_argument("--max-healthcheck-attempts",
-                        type=int,
-                        default=20,
-                        help="maximum number of attempts to connect to Scylla " +
-                        "before running the tests")
 
     args = parser.parse_args()
 
@@ -82,7 +56,7 @@ def main():
     print("Building tests")
     subprocess.run(args=["cargo", "build", "--tests"], check=True)
 
-    with scylla_docker(args.teardown, args.max_healthcheck_attempts) as node:
+    with scylla_docker(args.teardown) as node:
         print("Running tests")
         test_env = os.environ.copy()
         test_env["SCYLLA_URI"] = f"{node.ip}:{node.port}"
