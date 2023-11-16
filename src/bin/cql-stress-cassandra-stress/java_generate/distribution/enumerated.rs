7193534dc9cff06ab6ce313532c955aae7dad1dd
muzarski <mikolaj.uzarski@scylladb.com>
2023-11-16 16:25:19 +0100

    rng: introduce enumerated distribution
    Implemented `EnumeratedDistribution` which samples the items from the vector
    with the probability based on the weight assigned to each item.

---
+++ src/bin/cql-stress-cassandra-stress/java_generate/distribution/enumerated.rs
@@ -0,0 +1,33 @@
+use anyhow::Result;
+use rand_distr::{Distribution, WeightedIndex};
+
+#[derive(Clone)]
+pub struct EnumeratedDistribution<T> {
+    items: Vec<(T, f64)>,
+    dist: WeightedIndex<f64>,
+}
+
+impl<T: Copy> EnumeratedDistribution<T> {
+    pub fn new(items: Vec<(T, f64)>) -> Result<Self> {
+        let dist = WeightedIndex::new(items.iter().map(|w| w.1))?;
+
+        Ok(Self { items, dist })
+    }
+
+    pub fn sample(&self) -> T {
+        self.items[self.dist.sample(&mut rand::thread_rng())].0
+    }
+}
+
+impl<T: std::fmt::Display> std::fmt::Display for EnumeratedDistribution<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{")?;
+        let items_str = self
+            .items
+            .iter()
+            .map(|item| format!("{}={}", item.0, item.1))
+            .collect::<Vec<_>>()
+            .join(",");
+        write!(f, "{}}}", items_str)
+    }
+}
