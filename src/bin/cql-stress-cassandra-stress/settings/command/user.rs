6651ece204f28d4f9bba75a94f4677edbfe3a722
muzarski <mikolaj.uzarski@scylladb.com>
2024-03-08 11:56:52 +0100

    user: implement Parsable for UserProfile
    Implemented Parsable trait for UserProfile struct.

--- src/bin/cql-stress-cassandra-stress/settings/command/user.rs
+++ src/bin/cql-stress-cassandra-stress/settings/command/user.rs
@@ -1,6 +1,9 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File};
 
+use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
+
+use crate::settings::param::types::Parsable;
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
 #[serde(deny_unknown_fields)]
@@ -18,4 +21,20 @@ pub struct QueryDefinitionYaml {
     pub cql: String,
     pub consistency_level: Option<String>,
     pub serial_consistency_level: Option<String>,
+}
+
+impl Parsable for UserProfile {
+    type Parsed = Self;
+
+    fn parse(s: &str) -> Result<Self::Parsed> {
+        let yaml =
+            File::open(s).with_context(|| format!("Invalid profile yaml filepath: {}", s))?;
+        let profile: UserProfile =
+            serde_yaml::from_reader(yaml).context("Failed to parse profile yaml file")?;
+        anyhow::ensure!(
+            !profile.queries.is_empty(),
+            "'queries' map cannot be empty. Please define at least one query."
+        );
+        Ok(profile)
+    }
 }
