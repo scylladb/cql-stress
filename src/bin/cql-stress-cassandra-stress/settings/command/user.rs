80c7ffd022a069a791c2c12441b5aac34227bb74
muzarski <mikolaj.uzarski@scylladb.com>
2024-04-19 03:32:58 +0200

    user: QueryDefinitionYaml into QueryDefinition
    This commit introduces QueryDefinition struct. Beside this, it defines
    a function that converts a QueryDefinitionYaml into QueryDefinition,
    so the consistencies strings are parsed to rust enums.

--- src/bin/cql-stress-cassandra-stress/settings/command/user.rs
+++ src/bin/cql-stress-cassandra-stress/settings/command/user.rs
@@ -1,9 +1,12 @@
 use std::{collections::HashMap, fs::File};
 
 use anyhow::{Context, Result};
+use scylla::statement::{Consistency, SerialConsistency};
 use serde::{Deserialize, Serialize};
 
 use crate::settings::param::types::Parsable;
+
+use super::common::{ConsistencyLevel, SerialConsistencyLevel};
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
 #[serde(deny_unknown_fields)]
@@ -21,6 +24,32 @@ pub struct QueryDefinitionYaml {
     pub cql: String,
     pub consistency_level: Option<String>,
     pub serial_consistency_level: Option<String>,
+}
+
+impl QueryDefinitionYaml {
+    fn into_query_definition(self) -> Result<QueryDefinition> {
+        let cql = self.cql;
+        let consistency = self
+            .consistency_level
+            .map(|c| ConsistencyLevel::parse(&c))
+            .transpose()?;
+        let serial_consistency = self
+            .serial_consistency_level
+            .map(|sc| SerialConsistencyLevel::parse(&sc))
+            .transpose()?;
+
+        Ok(QueryDefinition {
+            cql,
+            consistency,
+            serial_consistency,
+        })
+    }
+}
+
+pub struct QueryDefinition {
+    pub cql: String,
+    pub consistency: Option<Consistency>,
+    pub serial_consistency: Option<SerialConsistency>,
 }
 
 impl Parsable for UserProfile {
