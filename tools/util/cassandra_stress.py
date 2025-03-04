6c85a6a59fda987c232b77e00c81aabedc714873
Dusan Malusev <dusan.malusev@scylladb.com>
2025-03-04 12:22:07 +0100

    feature(cassandra-stress): use ScyllaDB fork of Cassandra Stress instead of downloading Apache Cassandra
    Signed-off-by: Dusan Malusev <dusan.malusev@scylladb.com>

--- tools/util/cassandra_stress.py
+++ tools/util/cassandra_stress.py
@@ -6,10 +6,7 @@ from os.path import dirname
 from collections import namedtuple
 
 
-# scylla-tools-java is based on 3.* version of Apache Cassandra.
-# Notice that Apache c-s changed generation logic in version 4.0.0.
-# See: https://github.com/apache/cassandra/commit/f1f5f194620d3f9e11492f0051b6b71018033413
-DEFAULT_CASSANDRA_VERSION = "3.11.19"
+DEFAULT_CASSANDRA_STRESS_VERSION = "v3.17.3"
 ROOT_DIRECTORY = dirname(dirname(dirname(__file__)))
 DOWNLOAD_DIRECTORY_NAME = os.path.join(ROOT_DIRECTORY, "cassandra-download")
 
@@ -79,41 +76,63 @@ class CSCliRunner:
 
 
 class CassandraStress(CSCliRunner):
-    def __init__(self, cassandra_version=DEFAULT_CASSANDRA_VERSION):
-        cassandra_dir = f"apache-cassandra-{cassandra_version}"
-        cassandra_tar = f"{cassandra_dir}-bin.tar.gz"
+    def __init__(self, cassandra_stress_version=DEFAULT_CASSANDRA_STRESS_VERSION):
+        cassandra_stress_dir = "cassandra-stress"
+        cassandra_stress_tar = "cassandra-stress-bin.tar.gz"
 
-        abs_cassandra_dir = os.path.join(
-            DOWNLOAD_DIRECTORY_NAME, cassandra_dir)
-        abs_cassandra_tar = os.path.join(
-            DOWNLOAD_DIRECTORY_NAME, cassandra_tar)
+        abs_cassandra_dir = os.path.join(DOWNLOAD_DIRECTORY_NAME, cassandra_stress_dir)
+        abs_cassandra_tar = os.path.join(DOWNLOAD_DIRECTORY_NAME, cassandra_stress_tar)
+
+        super().__init__(
+            stress_cmd=[os.path.join(abs_cassandra_dir, "bin", "cassandra-stress")]
+        )
 
         if os.path.exists(abs_cassandra_dir):
             # Cassandra already fetched.
             print(
-                f"Cassandra {cassandra_version} already installed. Skipping the download phase.")
-        else:
+                f"Cassandra {cassandra_stress_version} already installed. Skipping the download phase."
+            )
+            return
+
+        if not os.path.exists(abs_cassandra_tar):
+            os.makedirs(DOWNLOAD_DIRECTORY_NAME, exist_ok=True)
             # Fetch cassandra.
             print(
-                f"Fetching cassandra {cassandra_version} to {DOWNLOAD_DIRECTORY_NAME}")
-            # https://dlcdn.apache.org/cassandra/3.11.18/apache-cassandra-3.11.18-bin.tar.gz
-            cassandra_url = f"https://dlcdn.apache.org/cassandra/{cassandra_version}/{cassandra_tar}"
-            subprocess.run(args=["wget", "-P", DOWNLOAD_DIRECTORY_NAME,
-                           "-N", "--no-verbose", cassandra_url], check=True)
+                f"Fetching cassandra {cassandra_stress_version} to {DOWNLOAD_DIRECTORY_NAME}"
+            )
+            cassandra_stress_url = f"https://github.com/scylladb/cassandra-stress/releases/download/{cassandra_stress_version}/cassandra-stress-bin.tar.gz"
+            subprocess.run(
+                args=[
+                    "wget",
+                    "-c",
+                    "-P",
+                    DOWNLOAD_DIRECTORY_NAME,
+                    "-N",
+                    "--no-verbose",
+                    cassandra_stress_url,
+                ],
+                check=True,
+            )
 
-            # Extract cassandra
-            print(f"Extracting cassandra {cassandra_version}")
-            subprocess.run(args=["tar", "-xzf", abs_cassandra_tar,
-                           "--directory", DOWNLOAD_DIRECTORY_NAME], check=True)
-            print(f"Extracted cassandra to {abs_cassandra_dir}")
-
-        stress_cmd = [os.path.join(
-            abs_cassandra_dir, "tools", "bin", "cassandra-stress")]
-        super().__init__(stress_cmd=stress_cmd)
+        # Extract cassandra
+        print(f"Extracting cassandra {cassandra_stress_version}")
+        subprocess.run(
+            args=[
+                "tar",
+                "-xzf",
+                abs_cassandra_tar,
+                "--directory",
+                DOWNLOAD_DIRECTORY_NAME,
+            ],
+            check=True,
+        )
+        print(f"Extracted cassandra to {abs_cassandra_dir}")
 
 
 class CqlStressCassandraStress(CSCliRunner):
     def __init__(self):
-        stress_cmd = ["cargo", "run", "--features", "user-profile", "--bin",
-                      "cql-stress-cassandra-stress", "--"]
-        super().__init__(stress_cmd=stress_cmd)
+        super().__init__(stress_cmd=["cql-stress-cassandra-stress"])
+
+
+if __name__ == "__main__":
+    cassandra_stress = CassandraStress()
