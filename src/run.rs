a844bf4874181b8d734d22ade5c3812459350472
Lukasz Sojka <lukasz.sojka@scylladb.com>
2025-05-12 18:57:29 +0200

    fix: fix latency precision for fixed rate
    `tokio::sleep_until` operates on millisecond granularity.
    In result, if the sleep time is <1ms, we may sleep longer than expected.
    
    Fix by using `tokio_timerfd` lib for precision sleep.
    
    fixes: https://github.com/scylladb/cql-stress/issues/134

--- src/run.rs
+++ src/run.rs
@@ -9,6 +9,7 @@ use futures::future::{AbortHandle, Abortable, Fuse, FutureExt};
 use futures::stream::{FuturesUnordered, StreamExt};
 use tokio::sync::oneshot;
 use tokio::time::Instant;
+use tokio_timerfd::Delay;
 
 use crate::configuration::{Configuration, OperationContext};
 
@@ -112,7 +113,7 @@ impl WorkerSession {
 
         let scheduled_start_time = if let Some(rate_limiter) = &self.context.rate_limiter {
             let start_time = rate_limiter.issue_next_start_time();
-            tokio::time::sleep_until(start_time).await;
+            let _ = Delay::new(start_time.into()).unwrap().await;
             start_time
         } else {
             Instant::now()
